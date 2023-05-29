7e31173bd75b0545d0cc5d115fcd462881ef0abb
Natalia Gracheva <ntlgracheva@gmail.com>
2023-05-29 19:25:15 +0200

    2error

--- task.js
+++ task.js
@@ -2,25 +2,27 @@
 
 const rectangle = {
   get width() {
-    return this._width = 5;
+    return this._width;
   },
   set width(a) {
     if (!isNaN(a)) { this._width = +a; }
+    else this._width = 5;
   },
 
   get height() {
-    return this._height = 5;
+    return this._height;
   },
   set height(b) {
-    if (!isNaN(b)) { this.height = +b; }
+    if (!isNaN(b)) { this._height = +b; }
+    else this._height = 5;
   },
 
   get perimetr() {
-    let perimetr = 2 * (this.width + this.height);
+    let perimetr = 2 * (this._width + this._height);
     return perimetr + "см"
   },
   get square() {
-    let square = this.width * this.height;
+    let square = this._width * this._height;
     return square + "см"
   }
 }
